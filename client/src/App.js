32b8f6c39440f71f02163deb2b72115a9be305da
necelentano <necelentano@gmail.com>
2021-07-03 01:14:44 +0300

    Routes and Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
-const App = () => (
-  <div>
-    <h1>React app</h1>
-    <p>Some text</p>
-  </div>
-);
+import { Switch, Route } from 'react-router-dom';
+
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  );
+};
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => (
+  <div>
+    <h1>React Home page</h1>
+  </div>
+);
+
+export default Home;
