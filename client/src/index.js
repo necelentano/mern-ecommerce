936a5b006be0735e11cdc584b44c86cdb2281fc1
necelentano <necelentano@gmail.com>
2021-07-06 00:37:23 +0300

    12. Register Form

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,8 +1,45 @@
+import { useState } from 'react';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBBtn,
+} from 'mdb-react-ui-kit';
+
 const Register = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    //
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <MDBInput
+        label="Type your email"
+        type="email"
+        className="form-control mt-4"
+        size="lg"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+      />
+      <MDBBtn type="submit" className="mt-4">
+        Register
+      </MDBBtn>
+    </form>
+  );
+
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <MDBContainer className="p-5">
+      <MDBRow>
+        <MDBCol size={6} className="offset-md-3">
+          <h4>Register</h4>
+          {registerForm()}
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 };
 
