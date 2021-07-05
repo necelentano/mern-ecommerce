02e56a474a295637854d19ae33ee16bad4d62779
necelentano <necelentano@gmail.com>
2021-07-05 22:58:35 +0300

    11. Firebase Setup

---
+++ client/src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBBkqKxvdXoJswWBLd6LG6qxuTBzTDwIZ4',
+  authDomain: 'mern-ecommerce-e4a93.firebaseapp.com',
+  projectId: 'mern-ecommerce-e4a93',
+  storageBucket: 'mern-ecommerce-e4a93.appspot.com',
+  messagingSenderId: '273072875762',
+  appId: '1:273072875762:web:b93966e5217a0d9e2f486b',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// export
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
