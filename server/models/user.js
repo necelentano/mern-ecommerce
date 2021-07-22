96bf1fbe1f0470c8f9263a17aa7b3dc8dcad4f4e
necelentano <necelentano@gmail.com>
2021-07-22 19:42:47 +0300

    33. User Schema

---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema;
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: true,
+      lowecase: true,
+      index: true,
+    },
+    role: {
+      type: String,
+      default: 'subscriber',
+    },
+    cart: { type: Array, default: [] },
+    address: String,
+    //whishlist: [{ type: ObjectId, ref: 'Product' }],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
