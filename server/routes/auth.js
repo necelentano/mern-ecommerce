743b62c0d701c21ab68bfd4a9c28c4158239b9dc
necelentano <necelentano@gmail.com>
2021-07-24 00:58:22 +0300

    35. Middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+// controllers
 const auth = require('../controllers/auth');
+
+// middlewares
+const { authCheck } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/create-or-update-user', auth.createOrUpdateUser);
+router.get('/create-or-update-user', authCheck, auth.createOrUpdateUser);
 
 module.exports = router;
