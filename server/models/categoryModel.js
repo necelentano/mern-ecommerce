c74725ab150adea42ce0e9d21d1ddbfd6b3aefc5
necelentano <necelentano@gmail.com>
2021-08-11 22:37:58 +0300

    49. Category Schema

---
+++ server/models/categoryModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      requred: true,
+      minLength: [3, 'Category name is too short'],
+      maxLength: [32, 'Category name is too long'],
+    },
+    slug: { type: String, unique: true, lowercase: true, index: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
