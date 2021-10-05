4c59a31f932b1c09496becb9670de3a85385f85e
necelentano <necelentano@gmail.com>
2021-10-05 03:36:38 +0300

    109. Fetch Products in Home Page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,31 @@
-const Home = () => (
-  <div>
-    <h1>React Home page</h1>
-  </div>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  getAllProductsAction,
+  clearAllProducts,
+} from '../store/actions/productActions';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { getProductsInProgress, allProducts } = useSelector(
+    (state) => state.product
+  );
+
+  useEffect(() => {
+    dispatch(getAllProductsAction(3));
+  }, []);
+
+  return (
+    <div>
+      <h1>React Home page</h1>
+      {getProductsInProgress ? (
+        <h2>LOADING ...</h2>
+      ) : (
+        JSON.stringify(allProducts)
+      )}
+    </div>
+  );
+};
 
 export default Home;
