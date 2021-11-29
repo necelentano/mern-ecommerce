265a80badb3e0b815fc1e36709455377a9565a84
necelentano <necelentano@gmail.com>
2021-11-29 02:54:13 +0200

    176. Cart Model

---
+++ server/models/cartModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema;
+
+const userSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: ObjectId,
+          ref: 'Product',
+        },
+        quantity: Number, // quantity of one product
+        price: Number,
+      },
+    ],
+    totalQuantity: Number, // total products in cart
+    totalPriceAfterDiscount: Number,
+    orderedBy: {
+      type: ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+const Cart = mongoose.model('Cart', userSchema);
+
+module.exports = Cart;
