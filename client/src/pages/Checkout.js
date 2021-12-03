08d2399a130af1ad43f89bce7f15c6dadbed210b
necelentano <necelentano@gmail.com>
2021-12-03 23:50:50 +0200

    180. Final Order Summary in Checkout

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -10,6 +10,7 @@ import {
   Input,
   Divider,
   Spin,
+  List,
 } from 'antd';
 
 import { getCartAction } from '../store/actions/cartActions';
@@ -23,107 +24,139 @@ const Checkout = () => {
     (state) => state.cart
   );
   const { user } = useSelector((state) => state.auth);
-  //
 
   useEffect(() => {
     dispatch(getCartAction(user.token));
   }, []);
 
+  const listItemsData =
+    cartFromDB.products.map((item) => ({
+      title: item.product.title,
+      price: item.price,
+      quantity: item.quantity,
+    })) || [];
   return (
-    <>
-      <Row>
-        <Col span={24}>
-          <Title
-            level={2}
-            style={{
-              color: 'black',
-              padding: '20px 0',
-              textAlign: 'center',
-              backgroundColor: '#b5f5ec',
-              fontSize: 30,
-            }}
+    cartFromDB && (
+      <>
+        <Row>
+          <Col span={24}>
+            <Title
+              level={2}
+              style={{
+                color: 'black',
+                padding: '20px 0',
+                textAlign: 'center',
+                backgroundColor: '#b5f5ec',
+                fontSize: 30,
+              }}
+            >
+              Checkout
+            </Title>
+          </Col>
+        </Row>
+        <Row>
+          <Col
+            xl={{ span: 18, offset: 3 }}
+            lg={{ span: 22, offset: 1 }}
+            md={{ span: 22, offset: 1 }}
+            xs={{ span: 22, offset: 1 }}
           >
-            Checkout
-          </Title>
-        </Col>
-      </Row>
-      <Row>
-        <Col
-          xl={{ span: 18, offset: 3 }}
-          lg={{ span: 22, offset: 1 }}
-          md={{ span: 22, offset: 1 }}
-          xs={{ span: 22, offset: 1 }}
-        >
-          <Row gutter={[16, 16]}>
-            <Col
-              xl={{ span: 12 }}
-              lg={{ span: 12 }}
-              md={{ span: 24 }}
-              xs={{ span: 24 }}
-            >
-              <Row>
-                <Title level={3}> Deliver Address</Title>
-              </Row>
-              <Row>
+            <Row gutter={[16, 16]}>
+              <Col
+                xl={{ span: 12 }}
+                lg={{ span: 12 }}
+                md={{ span: 24 }}
+                xs={{ span: 24 }}
+              >
+                <Row>
+                  <Title level={3}> Deliver Address</Title>
+                </Row>
+                <Row>
+                  <Space
+                    direction="vertical"
+                    size={30}
+                    style={{ width: '100%' }}
+                  >
+                    <TextArea
+                      rows={4}
+                      placeholder="Please enter your shipping address"
+                    />
+                    <Button type="primary">Save address</Button>
+                  </Space>
+                </Row>
+                <Divider />
+                <Row>
+                  <Title level={3}>Got Coupon?</Title>
+                </Row>
                 <Space direction="vertical" size={30} style={{ width: '100%' }}>
-                  <TextArea
-                    rows={4}
-                    placeholder="Please enter your shipping address"
-                  />
-                  <Button type="primary">Save address</Button>
+                  <Input placeholder="Paste coupon here if you have one" />
+                  <Button type="primary">Apply coupon</Button>
                 </Space>
-              </Row>
-              <Divider />
-              <Row>
-                <Title level={3}>Got Coupon?</Title>
-              </Row>
-              <Space direction="vertical" size={30} style={{ width: '100%' }}>
-                <Input placeholder="Paste coupon here if you have one" />
-                <Button type="primary">Apply coupon</Button>
-              </Space>
-            </Col>
-            <Col
-              xl={{ span: 12 }}
-              lg={{ span: 12 }}
-              md={{ span: 24 }}
-              xs={{ span: 24 }}
-            >
-              <Row>
-                <Title level={3}>Order Summary</Title>
-              </Row>
-              <hr />
-              {getCartFromDBInProgress && (
-                <div className="spiner">
-                  <Spin size="large" />
-                </div>
-              )}
-              {!getCartFromDBInProgress && cartFromDB.products.length && (
-                <>
-                  <Row>Products: {cartFromDB.products.length}</Row>
-                  <hr />
-                  <Row>List o products</Row>
-                  <hr />
-                  <Row>Cart total: ${cartFromDB.totalPrice}</Row>
-                  <hr />
+              </Col>
+              <Col
+                xl={{ span: 12 }}
+                lg={{ span: 12 }}
+                md={{ span: 24 }}
+                xs={{ span: 24 }}
+              >
+                <Row>
+                  <Title level={3}>Order Summary</Title>
+                </Row>
+                <hr />
+                {getCartFromDBInProgress && (
+                  <div style={{ width: 40, margin: '100px auto' }}>
+                    <Spin size="large" />
+                  </div>
+                )}
+                {!getCartFromDBInProgress && cartFromDB.products.length && (
+                  <>
+                    <Row>
+                      <Text strong>
+                        Products in cart: {cartFromDB.products.length}
+                      </Text>
+                    </Row>
+                    <hr />
+                    <Row>
+                      <List
+                        itemLayout="horizontal"
+                        dataSource={listItemsData}
+                        renderItem={(item) => (
+                          <List.Item>
+                            <Text>
+                              {item.title} x {item.quantity} = $
+                              {item.quantity * item.price}
+                            </Text>
+                          </List.Item>
+                        )}
+                      />
+                    </Row>
+                    <hr />
+                    <Row>
+                      <Text strong>
+                        Cart total price: ${cartFromDB.totalPrice}
+                      </Text>
+                    </Row>
+                    <hr />
 
-                  <Row style={{ marginBottom: 30 }}>
-                    <Space
-                      direction="horizontal"
-                      size={20}
-                      style={{ width: '100%' }}
-                      align="center"
-                    >
-                      <Button type="primary">Place Order</Button>
-                      <Button type="primary">Empty Cart</Button>
-                    </Space>
-                  </Row>
-                </>
-              )}
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </>
+                    <Row style={{ margin: '30px 0' }}>
+                      <Space
+                        direction="horizontal"
+                        size={20}
+                        style={{ width: '100%' }}
+                        align="center"
+                      >
+                        <Button type="primary">Place Order</Button>
+                        <Button type="primary">Empty Cart</Button>
+                      </Space>
+                    </Row>
+                  </>
+                )}
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </>
+    )
   );
 };
 
