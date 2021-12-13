71ea48b89a8886394bb65a9443aea1e7504382ff
necelentano <necelentano@gmail.com>
2021-12-13 20:49:22 +0200

    202. Order Schema

---
+++ server/models/orderModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema;
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: ObjectId,
+          ref: 'Product',
+        },
+        quantity: Number, // quantity of one product
+      },
+    ],
+    paymentIntetn: {},
+    orderStatus: {
+      type: String,
+      default: 'Not processing',
+      enum: [
+        'Not processing',
+        'Processing',
+        'Dispatched',
+        'Cancelled',
+        'Completed',
+      ],
+    },
+    orderedBy: {
+      type: ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
