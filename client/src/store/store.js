11936c20074621ca3123153361fa12145b3292fa
Андрей | Andrey <necelentano@gmail.com>
2023-08-07 11:55:16 +0300

    Removed Redux DevTools

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+//import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
@@ -12,7 +12,9 @@ const middlewares = [thunk.withExtraArgument(auth)];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(...middlewares), composeWithDevTools())
+  compose(applyMiddleware(...middlewares), 
+        //  composeWithDevTools()
+         )
 );
 
 export default store;
